cfd39602650d8e57957e2aa3b241a11bf0bfa6cb
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-03-10 16:18:23 -0500

    commit tree 5db4008d38c85e57887a04adcd752133d73fc17e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res => res.json()) 
+    .then((resData) => {
+        const body = document.querySelector("body");
+        body.append(resData.id);
+    })
+    .catch((error) => {
+        const body = document.querySelector("body");
+        body.append(error.message);
+    })
+
+}
+
+submitData("thomas", "thomas.com");
